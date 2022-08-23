b98e05c7ea8823709e30e8144c72ee86c4dbc895
Collins Kimani Karanja <kimcolinss@gmail.com>
2022-08-23 11:22:37 +0300

    Done assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = 31*2;
+const random = Math.floor(Math.random()*10)+1;
+let num3 = 16;
+let num4 = 6;
+const mod = 16%6;
+const max = Math.max(20);
